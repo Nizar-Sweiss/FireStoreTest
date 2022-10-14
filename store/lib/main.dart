c6ee316b5ecc5a99bd1d8a0a1c20013a1706fa7b
Nizar Sweiss <Nizarsweiss00@gmail.com>
2022-10-14 12:36:04 +0300

    add: redirect main to the signup Screen

--- store/lib/main.dart
+++ store/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'Screens/screens.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -22,6 +22,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SignIn();
   }
 }
